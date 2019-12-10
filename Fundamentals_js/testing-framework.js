35586449fe02f27e8384b70d0b5f599117a53390
yasser <yassergeo@gmail.com>
2019-12-09 21:40:29 -0500

    added framework

---
+++ Fundamentals_js/testing-framework.js
@@ -0,0 +1,38 @@
+const {sum, subtract} = require('./math')
+
+test('sum add sumTest', async () => {
+  const result = await sum (4 , 7)
+  const expected = 10
+  expect(result).toBe(expected)
+})
+
+test('subtract subtracts subtracTest', async () => {
+  const result = await subtract (7 , 3)
+  const expected = 4
+  expect(result).toBe(expected)
+})
+
+
+async function test (title, callback) {
+  try{
+    await callback()
+    console.log(`✓ ${title}`);
+    
+  } catch (error){
+    console.error(`+ ${title}`);
+    console.error(error);
+  }
+}
+
+
+function expect (actual) {
+  return {
+    toBe (expected) {
+      if(actual !== expected){
+        throw new Error(`${result} is not equal to ${expected}`);
+      }
+    },
+    toEqual(expected) {},
+    toBeGreaterThan(expected) {}
+  }
+}
